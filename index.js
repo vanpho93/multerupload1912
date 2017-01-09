5904111e537c69d3a72d89eea7a4260deaca43a9
Admin <vanpho02@gmail.com>
2017-01-09 19:58:30 +0700

    setup storage

--- index.js
+++ index.js
@@ -5,3 +5,18 @@ app.set('views', './views');
 app.listen(3000, () => console.log('Server started'));
 
 app.get('/', (req, res) => res.render('home'));
+
+var multer = require('multer');
+var storage = multer.diskStorage({
+  destination(req, file, cb){
+    cb(null, './public');
+  },
+  filename(req, file, cb){
+    cb(null, Date.now() + file.originalname)
+  }
+});
+var upload = multer({storage});
+
+app.post('/xulydangky', upload.single('avatar'), (req, res) => {
+  
+});
