83e02044b33a70344571a539663cdd96501a7d79
Admin <vanpho02@gmail.com>
2017-01-11 19:54:17 +0700

    toi thu 4

--- index.js
+++ index.js
@@ -7,3 +7,6 @@ app.listen(3000, () => console.log('Server started'));
 
 app.get('/', (req, res) => res.render('home'));
 app.post('/xulydangky', require('./controller/xulydangky.js'));
+
+app.get('/array', (req, res) => res.render('array'));
+app.post('/xulymang', require('./controller/xulymang.js'));
